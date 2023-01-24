e0d127bcc1fa9313c570175c9fa16d82a422d749
shambuwu <balfappius@protonmail.ch>
2023-01-24 19:39:36 +0100

    IntroOverlay.tsx component added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import IntroOverlay from "./components/IntroOverlay";
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <IntroOverlay/>
     </div>
   );
 }
---
+++ src/components/IntroOverlay.tsx
@@ -0,0 +1,9 @@
+import '../App.css'
+
+export default function IntroOverlay(props) {
+    return(
+        <section>
+            <h1>test</h1>
+        </section>
+    )
+}
