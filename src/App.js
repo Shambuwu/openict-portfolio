56cdce868e89acf48934a357f2eb63e6f7bf0f9f
shambuwu <balfappius@protonmail.ch>
2023-01-29 22:34:36 +0100

    Pre-pages update

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
-import {createTheme, ThemeProvider, CssBaseline, Backdrop, CircularProgress} from "@mui/material";
-import Navbar from "./components/Navbar.tsx"
-import Profile from "./components/Profile.tsx"
-import Projects from "./components/Projects.tsx"
-import Skills from "./components/Skills.tsx"
-import Competences from "./components/Competences.tsx"
-import CustomDivider from "./components/CustomDivider.tsx";
-import About from "./components/About.tsx";
-import {useEffect, useRef, useState} from "react";
-import DarkModeContext from "./hooks/DarkModeContext.tsx";
-import RefContext from "./hooks/RefContext.tsx";
+import Profile from "./components/Profile";
+import CustomDivider from "./components/CustomDivider";
+import About from "./components/About";
+import Skills from "./components/Skills";
+import Projects from "./components/Projects";
+import Competences from "./components/Competences";
+import {createTheme, CssBaseline, ThemeProvider} from "@mui/material";
+import DarkModeContext from "./hooks/DarkModeContext";
+import {useState} from "react";
+import Navbar from "./components/Navbar";
+import Qualities from "./components/Qualities";
 
 const font = "'Space Mono', monospace"
 
 function App() {
     const [darkMode, setDarkMode] = useState("dark");
-    const ref = useRef();
 
     const theme = createTheme({
         palette: {
@@ -36,28 +35,26 @@ function App() {
         }
     })
 
-  return (
-      <ThemeProvider theme={theme}>
-          <CssBaseline />
-          <div className="App">
-              <DarkModeContext.Provider value={[darkMode, setDarkMode]}>
-                  <Navbar />
-              </DarkModeContext.Provider>
-              <Profile />
-              <CustomDivider text={"Wat heb ik gedaan?"} />
-              <About />
-              <CustomDivider text={"Wat heb ik geleerd?"} />
-              <RefContext.Provider value={ref}>
-                  <Skills ref={ref}/>
-              </RefContext.Provider>
-              <CustomDivider text={"Wat heb ik gemaakt?"} />
-              <Projects />
-              <CustomDivider text={"Wat heb ik geleerd?"} />
-              <Competences />
-              <CustomDivider text={"Wat heb ik geleerd?"} />
-          </div>
-      </ThemeProvider>
-  );
+    return (
+        <div className={"App"}>
+            <ThemeProvider theme={theme}>
+                <CssBaseline />
+                <DarkModeContext.Provider value={[darkMode, setDarkMode]}>
+                    <Navbar />
+                </DarkModeContext.Provider>
+                <Profile />
+                <CustomDivider text={"Wat heb ik gedaan?"} />
+                <About />
+                <CustomDivider text={"Wat heb ik geleerd?"} />
+                <Skills />
+                <CustomDivider text={"Wat heb ik gemaakt?"} />
+                <Projects />
+                <CustomDivider text={"Welke kwaliteiten heb ik laten zien?"} />
+                <Qualities />
+                <CustomDivider text={"Wat heb ik geleerd?"} />
+            </ThemeProvider>
+        </div>
+    )
 }
 
 // <main>
---
+++ src/components/Qualities.tsx
@@ -0,0 +1,5 @@
+export default function Qualities() {
+    return (
+        <p>test</p>
+    )
+}
